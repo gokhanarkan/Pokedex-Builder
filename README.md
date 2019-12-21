3f41f928c9b2364686212a1b91af2bee0c563816
Gokhan <gokhanarkan@gmail.com>
2019-12-21 15:43:29 +0300

    Update 0.2 || Now added some significant information in tables

--- README.md
+++ README.md
@@ -1,14 +1,34 @@
 # Pokedex-Builder
 
-I decided to write my own minimalistic Pokédex. You never know you need until you need it.
-
-A day ago, I published this repo, and it gave me the idea to scrap the necessary information.
-
-Although the application is private and only for me, I am sharing the scraping algorithm and current data publicly for everyone to use.
-
-Note that this is an ongoing project and the current data set is pretty basic. I will try to add more data and create connections with fields soon.
-
-Huge thanks to https://pokemondb.net for providing the information.
+---
 
 Install packages: ```pip install -r requirements.txt```
 Run: ```python src/pokedex_builder.py```
+
+---
+
+I decided to write my own minimalistic Pokédex. You never know you need until you need it.
+
+A day ago, I published [this repo](https://github.com/gokhj/get_pokemon_names), and it gave me the idea to scrape the necessary information from different resources to create my own database.
+
+Although the pokedex app is currently private since it's in development, I am sharing the scraping algorithm and current data publicly for everyone to use.
+
+Note that this is an ongoing project and the current data set is expanding regularly.
+
+I will add a client file to create objects, access generic data soon.
+
+Huge thanks to https://pokemondb.net for providing the information.
+
+---
+
+# UPDATES
+
+###### 21/12/2019
+I now added a lot of useful information from tables. The csv file contains more than enough data for my app but I will try to scrape more in the future. At this time, I will create a client file to access the information easily for other people.
+
+---
+
+# TODO
+
+- Some data in tables missing for gen8. Pokemondb didn't provide these details yet, I'll try to find external source for similar information.
+- Evolution levels are missing. I will also update that field with before/after information and possibly stone evolutions.
--- src/pokedex_builder.py
+++ src/pokedex_builder.py
@@ -24,10 +24,13 @@ def export_csv(poke_dict):
 def make_html(pokemon):
 
 	# Extra caution for nidoran genders and flabebe
-	if pokemon == "Nidoran♀": pokemon = "nidoran-f"
-	if pokemon == "Nidoran♂": pokemon = "nidoran-m"
-	if pokemon == "Flabébé": pokemon = "flabebe"
-		
+	if pokemon == "Nidoran♀":
+		pokemon = "nidoran-f"
+	if pokemon == "Nidoran♂":
+		pokemon = "nidoran-m"
+	if pokemon == "Flabébé":
+		pokemon = "flabebe"
+
 	# Extra caution for bloody farfetch'd and his mates
 	namelist = list(pokemon)
 	if "'" in namelist:
@@ -36,7 +39,8 @@ def make_html(pokemon):
 		for chars in oldname:
 			pokemon += chars
 
-	if pokemon == "Mime Jr.": pokemon = "mime-jr"
+	if pokemon == "Mime Jr.":
+		pokemon = "mime-jr"
 	# Extra caution for Sir Mr. Mime and his bros
 	elif "." in namelist:
 		oldname = pokemon.split(". ")
@@ -45,42 +49,94 @@ def make_html(pokemon):
 	elif ":" in namelist:
 		oldname = pokemon.split(": ")
 		pokemon = oldname[0] + "-" + oldname[1]
-		# Tapu Pokemon
+	# Tapu Pokemon
 	elif " " in namelist:
 		oldname = pokemon.split(" ")
 		pokemon = oldname[0] + "-" + oldname[1]
 
 	# Reaching the page and getting HTML entities
 	user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-	headers={'User-Agent':user_agent,}
-	url = Request("https://pokemondb.net/pokedex/{}".format(pokemon), None, headers)
-	
+	headers = {'User-Agent': user_agent, }
+	url = Request(
+		"https://pokemondb.net/pokedex/{}".format(pokemon), None, headers)
+
 	return urlopen(url)
 
 # THIS METHOD GETS STRING WITH HTML CODE AND CLEANS WITH REGULAR EXPRESSION
 def clean_tags(raw_html):
+
 	cleanr = re.compile('<.*?>')
 	cleantext = re.sub(cleanr, '', raw_html)
 	return cleantext
 
 # THIS METHOD SCRAPES ALL THE POKEMONS CREATED FROM THE DIRECTORY
 def get_pokemon_names():
+
 	name_dict = []
 	html = make_html("national")
 	# Converting into a BeautifulSoup object and iterating the content
 	soup = BeautifulSoup(html.read(), "html5lib")
-	for span in soup.findAll('span', attrs={'class':'infocard-lg-data'}):
-		for name in span.findAll('a', attrs={'class':'ent-name'}):
+	for span in soup.findAll('span', attrs={'class': 'infocard-lg-data'}):
+		for name in span.findAll('a', attrs={'class': 'ent-name'}):
 			# Pokemon Name
 			name_dict.append(name.text)
 
 	return name_dict
 
+# THIS METHOD CREATES TABLE OUT OF MESSY LIST OF INFORMATION
+def make_table(cleaned_table):
+
+	true_table = limit_table(cleaned_table)
+
+	pokedex_data = []
+	training = []
+	breeding = []
+	base_stats = []
+
+	# Addint information to Pokedex Data table
+	for p in range(0, 14):
+		pokedex_data.append(true_table[p])
+
+	# Adding information to Training table
+	for t in range(14, 24):
+		training.append(true_table[t])
+
+	# Adding information to Breeding table
+	for b in range(24, 30):
+		breeding.append(true_table[b])
+
+	# Adding information to Base Stats table
+	# Note: I grouped them up in four; header, base stat, min and max
+	for bs in range(30, 54, 4):
+		if bs == len(true_table)-2:
+			total = [true_table[bs], true_table[bs+1]]
+			base_stats.append(total)
+			break
+		stat = [true_table[bs], true_table[bs+1], true_table[bs+2], true_table[bs+3]]
+		base_stats.append(stat)
+
+	return pokedex_data, training, breeding, base_stats
+
+# THIS METHOD LIMITS THE LENGTH OF THE TABLE
+# ALTHOUGH THE FURTHER INFORMATION IS NOT NECESSARY FOR ME AT THE MOMENT, YOU CAN COMMENT THE BREAK AND ACCESS ALL THE DATA
+def limit_table(cleaned_table):
+
+	true_table = []
+	for array in cleaned_table:
+		for product in array:
+			if len(true_table) == 56:
+				break
+			if product != '':
+				true_table.append(product)
+
+	return true_table
+
 # THIS METHOD SCRAPES THE IMPORTANT INFORMATION FROM POKEDEX PAGE
 def get_pokemon_details(name_dict):
+
 	counter = 1
 	for pokemon in name_dict:
-		
+
 		print(pokemon)
 		# Create the html file to start for single pokedex entry
 		html = make_html(pokemon)
@@ -94,13 +150,13 @@ def get_pokemon_details(name_dict):
 
 		# EVOLUTION NAMES OF THE POKEMON
 		evolution = []
-		for span in soup.findAll('span', attrs={'class':'infocard-lg-data'}):
-			for name in span.findAll('a', attrs={'class':'ent-name'}):
+		for span in soup.findAll('span', attrs={'class': 'infocard-lg-data'}):
+			for name in span.findAll('a', attrs={'class': 'ent-name'}):
 				evolution.append(name.get_text())
 
 		# IMAGES OF THE POKEMON
 		img = []
-		for span in soup.findAll('span', attrs={'class':'infocard-lg-img'}):
+		for span in soup.findAll('span', attrs={'class': 'infocard-lg-img'}):
 			for image in span.findAll('a'):
 				src = str(image).split('"')[7]
 				img.append(src)
@@ -112,28 +168,41 @@ def get_pokemon_details(name_dict):
 
 		# MAIN IMAGE OF THE POKEMON
 		try:
-			for artwork in soup.find('a', attrs={'rel':'lightbox'}):
+			for artwork in soup.find('a', attrs={'rel': 'lightbox'}):
 				main_image = artwork['src']
 		except:
 			artwork = soup.find('img')
 			main_image = artwork['src']
 
-		arrange_data(pokemon, definition, evo_and_image, main_image, counter)
+		# VARIOUS TABLE DATA TABLE
+		table_data = []
+		# This for loop checks all the fields for table data like breeding, capture chance, type etc.
+		for poketype in soup.findAll('table', attrs={'class': 'vitals-table'}):
+			if poketype != "\n":
+				table_data.append(clean_tags(str(poketype)))
+		# There will be tuples of data and it needs some cleaning to put all of them into a single list
+		cleaned_table = []
+		for t in table_data:
+			x = t.split('\n')
+			cleaned_table.append(x)
+
+		pokedex_data, training, breeding, base_stats = make_table(cleaned_table)
+
+		arrange_data(pokemon, definition, evo_and_image, main_image,
+		             counter, pokedex_data, training, breeding, base_stats)
 		counter += 1
 
 # THIS METHOD MODIFIES THE DATA FOR FINAL ENTRY
-def arrange_data(pokemon, definition, evo_and_image, main_image, counter):
+def arrange_data(pokemon, definition, evo_and_image, main_image, counter, pokedex_data, training, breeding, base_stats):
 
 	if len(evo_and_image) < 1:
 		evo_and_image = "No Evolution"
 
-	arranged_data = [counter, pokemon, definition, main_image, evo_and_image]
+	arranged_data = [counter, pokemon, definition, main_image,
+                  evo_and_image, pokedex_data, training, breeding, base_stats]
 	poke_dict.append(arranged_data)
 	# pokemon = Pokemon(counter, pokemon, definition, main_image, evo_and_image)
 
 if __name__ == '__main__':
-	try:
-		get_pokemon_details(get_pokemon_names())
-		export_csv(poke_dict)
-	except Exception as e:
-		print(e)
+	get_pokemon_details(get_pokemon_names())
+	export_csv(poke_dict)
